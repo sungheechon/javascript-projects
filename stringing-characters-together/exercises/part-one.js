13572e2cc5e5a59817e1eb7337dbc71309c667b0
sunghee chon <sungheechon@sunghees-imac.lan>
2023-10-30 19:59:10 -0400

    Chapter 7 Exercises: Strings Part 1-3

--- stringing-characters-together/exercises/part-one.js
+++ stringing-characters-together/exercises/part-one.js
@@ -4,7 +4,20 @@ let num = 1001;
 console.log(num.length);
 
 //Use type conversion to print the length (number of digits) of an integer.
+num = String(num);
+console.log(num.length);
 
 //Follow up: Print the number of digits in a DECIMAL value (e.g. num = 123.45 has 5 digits but a length of 6).
+num = 123.45;
+
+console.log(`num = ${num} has ${String(num).length - 1} digits but a length of ${String(num).length}.`)
 
 //Experiment! What if num could be EITHER an integer or a decimal?  Add an if/else statement so your code can handle both cases.
+
+if (String(num).indexOf(".")) {
+    console.log("decimal");
+    console.log(String(num).length-1);
+} else {
+    console.log("integer");
+    console.log(String(num).length);
+}
--- stringing-characters-together/exercises/part-three.js
+++ stringing-characters-together/exercises/part-three.js
@@ -3,15 +3,24 @@
 let language = 'JavaScript';
 
 //1. Use string concatenation and two slice() methods to print 'JS' from 'JavaScript'
+console.log(language.slice(0,1)+language.slice(4,5));
 
 //2. Without using slice(), use method chaining to accomplish the same thing.
+console.log(language[0] + language[4]);
 
 //3. Use bracket notation and a template literal to print, "The abbreviation for 'JavaScript' is 'JS'."
+console.log(`The abbreviation for ${language} is ${language[0]}${language[4]}.`);
 
 //4. Just for fun, try chaining 3 or more methods together, and then print the result.
+console.log(language[0] + language[4] + language.slice(-1));
 
 //Part Three section Two
 
 //1. Use the string methods you know to print 'Title Case' from the string 'title case'.
 
 let notTitleCase = 'title case';
+
+notTitleCase = notTitleCase.replace(notTitleCase[0], notTitleCase[0].toUpperCase());
+notTitleCase = notTitleCase.replace(notTitleCase[6], notTitleCase[6].toUpperCase());
+
+console.log(notTitleCase);
