724598d18467b925c192504be47b4dd147193184
sunghee chon <sungheechon@sunghees-imac.lan>
2023-11-04 22:30:55 -0400

    Studio 7 & 8: Strings and Arrays

--- arrays/studio/array-string-conversion/array-testing.js
+++ arrays/studio/array-string-conversion/array-testing.js
@@ -11,9 +11,13 @@ strings.concat()
 
 function reverseCommas() {
 	//TODO: 1. create and instantiate your variables.
-	let check;
+	let check = ",";
 	let output;
 	//TODO: 2. write the code required for this step
+
+	if (strings[0].includes(check)) {
+		output = strings[0].split(check).reverse().join(check);
+	}
 
 	//NOTE: For the code to run properly, you must return your output.  this needs to be the final line of code within the function's { }.
 	return output;
@@ -21,37 +25,43 @@ function reverseCommas() {
 
 //3)
 function semiDash() {
-	let check;
+	let check = ";";
 	let output;
-//TODO: write the code required for this step
+	//TODO: write the code required for this step
+	if (strings[1].includes(check)) {
+		output = strings[1].split(check).sort().join("-");
+	}
 
-  
 	return output;
 }
 
 //4)
 function reverseSpaces() {
-	let check;
+	let check = " ";
 	let output;
-  //TODO: write the code required for this step
-
+	//TODO: write the code required for this step
+	if (strings[2].includes(check)) {
+		output = strings[2].split(check).sort().reverse().join(check);
+	}
 	return output;
 }
 
 //5)
 function commaSpace() {
-	let check;
+	let check = ", ";
 	let output;
 	//TODO: write the code required for this step
-  
+	if (strings[3].includes(check)) {
+		output = strings[3].split(check).reverse().join(check[0]);
+	}
 	return output;
 }
 
 // NOTE: Don't add or modify any code below this line or your program might not run as expected.
 module.exports = {
-	strings : strings, 
-	reverseCommas : reverseCommas,
-	semiDash: semiDash, 
-	reverseSpaces : reverseSpaces,
-	commaSpace : commaSpace
+	strings: strings,
+	reverseCommas: reverseCommas,
+	semiDash: semiDash,
+	reverseSpaces: reverseSpaces,
+	commaSpace: commaSpace
 };
--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -1,27 +1,25 @@
 const input = require('readline-sync');
 let str = "LaunchCode";
-console.log(str.length);
-
-let removedStr = str.slice(0, 3); // first 3 characters
-let remainedStr = str.slice(3);  // - removedStr
-let changedStr = remainedStr.concat(removedStr);
-// new variable
 
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
-console.log(changedStr);
+let newStr = str.slice(3) + str.slice(0, 3);
 
 //Use a template literal to print the original and modified string in a descriptive phrase.
-console.log(`${changedStr} does not make sense. It needs to be ${str}.`);
+
+console.log(`Changed the "${str}" to "${newStr}" by removing the first 3 characters and adding to the end.`)
 
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
 
-let userInputNumber = input.question("How many letters do we need to change? ");
+let userInputNum = input.question("Enter the number of letters to be relocated: ");
 
 //3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
-// Number(userInputNumber);
-
-if (Number(userInputNumber) > Number(str.length)) {
-    console.log(changedStr);
-    console.log(`Your number "${userInputNumber}" does not make sense. It needs to be less than ${str.length}.`);
-} 
+if (userInputNum > str.length) {
+    console.log(`Your number, "${userInputNum}" is longer than the word, "${str}" which has "${str.length}" letters. So it's defaulted to moving 3 characters to the end. "${newStr}".`)
+} else if (userInputNum < 1) {
+    console.log(`Your number, "${userInputNum}" is not a valid number. So it's defaulted to moving 3 characters to the end. "${newStr}".`)
+}
+else {
+    let userInputStr = str.slice(userInputNum) + str.slice(0, userInputNum);
+    console.log(`You asked for "${userInputNum}" of the letters to be moved. The word, "${str}" is now changed to "${userInputStr}".`)
+}
