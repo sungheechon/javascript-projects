3c19490c2274fcafacf5ef2f38d95220a6acf51f
sunghee chon <sungheechon@sunghees-imac.lan>
2023-11-14 15:47:40 -0500

    CH 17 - Exercises

--- exceptions/exercises/divide.js
+++ exceptions/exercises/divide.js
@@ -1,6 +1,15 @@
 // Write a function called 'divide' that takes two parameters: a numerator and a denominator.
+function divideTwoParam (numer, denom) {
+    dividedNum = 0;
+    if (denom === 0) {
+        throw Error("Attempted to divide by zero.");
+    }
+  
+    return devideNum = numer / denom;
+}
 
 // Your function should return the result of numerator / denominator.
+console.log(divideTwoParam(4, 2));
 
 // However, if the denominator is zero you should throw the error, "Attempted to divide by zero."
 
