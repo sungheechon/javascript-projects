8b087d72a87b7ebe406d0cd51e34377b583525f6
sunghee chon <sungheechon@sunghees-imac.lan>
2023-11-30 11:01:51 -0500

    CH 23 Exercises: The DOM and Events

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,25 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
+    button.addEventListener("click", function (event) {
+        paragraph.innerHTML = "Houston, we have liftoff!";
+    })
+
+    missionAbort.addEventListener("mouseover", function (event) {
+        event.target.style.backgroundColor = "red";
+    })
     
+    missionAbort.addEventListener("mouseout", function (event) {
+        event.target.style.backgroundColor = "";
+    })
+
+    missionAbort.addEventListener("click", function (event) {
+        let confirmMsg = "Are you sure you want to abort the mission?";
+        let confirmMsgTrue = "Mission aborted! Space shuttle returning home";
+        if (window.confirm(confirmMsg)) {
+            paragraph.innerHTML = confirmMsgTrue;
+        }
+    })
 }
 
 window.addEventListener("load", init);
